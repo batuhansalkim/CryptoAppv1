d3d6600afeaf0bb850064722f89134419e9b22e9
Baturpcu <batusalkim11@gmail.com>
2023-04-09 13:40:04 +0300

    update

--- newcoin/src/routes/Coin.js
+++ newcoin/src/routes/Coin.js
@@ -2,6 +2,7 @@ import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import "./Coin.css";
+import DOMPurify from 'dompurify'
 
 const Coin = () => {
     const params = useParams()
@@ -29,8 +30,68 @@ const Coin = () => {
                 <div className="info">
                     <div className="coin-heading">
                         {coin.image ? <img src={coin.image.small} alt=''/> : null}
+                        <p>{coin.name}</p>
+                        <p>{coin.symbol}</p>
+                    </div>
+                        <div className="coin-price">
+                          {coin.market_data?.current_price ? <h1>${coin.market_data.current_price.usd.toLocaleString()}</h1> : null}
+                        </div>
+                </div>
+            </div>
+            <div className="content">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>1h</th>
+                            <th>24h</th>
+                            <th>7d</th>
+                            <th>14d</th>
+                            <th>30d</th>
+                            <th>1yr</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{coin.market_data?.price_change_percentage_1h_in_currency ? <p>{coin.market_data.price_change_percentage_1h_in_currency.usd.toFixed(1)}%</p> : null}
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div className="content">
+                <div className="stats">
+                    <div className="left">
+                        <div className="row">
+                            <h4>24 Hour Low</h4>
+                              {coin.market_data?.low_24h ? <p>${coin.market_data.low_24h.usd.toLocaleString()}</p> : null}
+                        </div>
+                        <div className="row">
+                              <h4>24 Hour High</h4>
+                              {coin.market_data?.high_24h ? <p>${coin.market_data.high_24h.usd.toLocaleString()}</p> : null}
+                        </div>
+                    </div>
+                    <div className="right">
+                          <div className='row'>
+                              <h4>Market Cap</h4>
+                              {coin.market_data?.market_cap ? <p>${coin.market_data.market_cap.usd.toLocaleString()}</p> : null}
+                          </div>
+                          <div className='row'>
+                              <h4>Circulating Supply</h4>
+                              {coin.market_data ? <p>{coin.market_data.circulating_supply}</p> : null}
+                          </div>
                     </div>
                 </div>
+            </div>
+            <div className="content">
+                  <div className='about'>
+                      <h3>About</h3>
+                      <p dangerouslySetInnerHTML={{
+                          __html: DOMPurify.sanitize(coin.description ? coin.description.en : ''),
+                      }}>
+
+                      </p>
+
+                  </div>
             </div>
         </div>
     </div>
