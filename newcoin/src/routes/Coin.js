0ecf1acd78866c339c1d8d98044990e810382874
Baturpcu <batusalkim11@gmail.com>
2023-04-09 12:08:09 +0300

    update

--- newcoin/src/routes/Coin.js
+++ newcoin/src/routes/Coin.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+
 
 const Coin = () => {
+    const url = `https://api.coingecko.com/api/v3/coins/${params.coinId}`
+
   return (
     <div>
         <h1>baty</h1>
