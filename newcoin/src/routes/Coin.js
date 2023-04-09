3b8ea393b3b1d53494eb0726fbdf0b8380ae85a7
Baturpcu <batusalkim11@gmail.com>
2023-04-09 12:32:15 +0300

    update

--- newcoin/src/routes/Coin.js
+++ newcoin/src/routes/Coin.js
@@ -1,14 +1,38 @@
 import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-
+import "./Coin.css";
 
 const Coin = () => {
+    const params = useParams()
+    const [coin,setCoin] = useState({});
     const url = `https://api.coingecko.com/api/v3/coins/${params.coinId}`
+
+    useEffect(()=>{
+        axios.get(url).then((res)=>{
+            setCoin(res.data);
+        }).catch((error)=>{
+            console.log(error);
+        })
+    },[])
 
   return (
     <div>
-        <h1>baty</h1>
+        <div className="coin-container">
+            <div className="content">
+                <h1>{coin.name}</h1>
+            </div>
+            <div className='content'>
+                <div className="rank">
+                    <span className='rank-btn'>Rank # {coin.market_cap_rank}</span>
+                </div>
+                <div className="info">
+                    <div className="coin-heading">
+                        {coin.image ? <img src={coin.image.small} alt=''/> : null}
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   )
 }
--- newcoin/src/routes/deneme.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function deneme() {
-  return (
-    <div>deneme</div>
-  )
-}
-
-export default deneme
